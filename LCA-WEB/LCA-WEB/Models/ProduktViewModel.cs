9d02e91032bdeb66626f0e448e97efb24b792624
S0543830 <s0543830@htw-berlin.de>
2017-07-08 19:15:41 +0200

    Repariert

---
+++ LCA-WEB/LCA-WEB/Models/ProduktViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LCA_WEB.Models
+{
+    public class ProduktViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Durability { get; set; }
+        public double TotalWeight { get; set; }
+        public System.DateTime DateOfCreation { get; set; }
+        public System.DateTime DateOfChanging { get; set; }
+        public string CreatedBy { get; set; }
+        public string ChangeBy { get; set; }
+        public Nullable<int> Typ_Id { get; set; }
+    }
+}
